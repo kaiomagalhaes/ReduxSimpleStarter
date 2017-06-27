8e6cbc45a986d269685ece8b81cfc9ca9b68e2f8
Kaio Magalhães <me@kaiomagalhaes.com>
2017-06-27 19:00:22 -0300

    finished first part

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,45 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import SearchBar from './search_bar'
+import VideoList from './video_list'
+import VideoDetail from './video_detail'
+import _ from 'lodash'
+
+import YTSearch from 'youtube-api-search'
+
+const API_KEY = 'AIzaSyD92FMy4JyRnLak_HHZ1B74sD6UAvB-ViE'
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { 
+      videos: [],
+      selectedVideo: null
+    }
+    this.videoSearch('dragon ball z')
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      })
+    })
+  }
+
   render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300)
     return (
-      <div></div>
-    );
+      <div>
+        <SearchBar onSearchTermChange={videoSearch }/>
+        <VideoDetail 
+          video={this.state.selectedVideo} 
+        />
+        <VideoList 
+          videos={this.state.videos}
+          onVideoSelect={selectedVideo => { this.setState({selectedVideo})}}
+        />
+      </div>
+    )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,42 +1,22 @@
-/*
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-
-import App from './components/app';
-import reducers from './reducers';
-
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
-  */
-
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
 import App from './components/app'
 
 ReactDOM.render(
-  <AppContainer>
-    <App/>
-  </AppContainer>,
-  document.querySelector('.container')
+	<App />,
+	document.querySelector('.container')
 );
 
 // Hot Module Replacement API
 if (module.hot) {
-  module.hot.accept('./components/app', () => {
-    const NextApp = require('./components/app').default;
-    ReactDOM.render(
-      <AppContainer>
-        <NextApp/>
-      </AppContainer>,
-      document.querySelector('.container')
-    );
-  });
+	module.hot.accept('./components/app', () => {
+		const NextApp = require('./components/app').default;
+		ReactDOM.render(
+			<AppContainer>
+				<NextApp/>
+			</AppContainer>,
+			document.querySelector('.container')
+		);
+	});
 }
